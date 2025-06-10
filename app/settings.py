a55d21c64a60c03066f5aa9935b013a0c1d6c03f
Robson Júnior <robsonlopes.jr@gmail.com>
2025-06-10 17:27:48 -0300

    add app authentication e geração de token jwt

--- app/settings.py
+++ app/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'authentication',
     'genres',
     'actors',
     'movies',
-    'reviews'
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
