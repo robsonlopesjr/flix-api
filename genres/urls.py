9c4484af8ac111d05e1eba6a992a1fa84f3181da
Robson Júnior <robsonlopes.jr@gmail.com>
2025-06-10 10:08:16 -0300

    método para detalhar um gênero

--- genres/urls.py
+++ genres/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from genres.views import genre_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 urlpatterns = [
-    path('', genre_view, name='genre'),
+    path('', genre_create_list_view, name='genre-create-list'),
+    path('/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 ]
