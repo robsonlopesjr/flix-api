95c87be14be7f62ae5dccd624f6515d720e0bcfc
Robson Júnior <robsonlopes.jr@gmail.com>
2025-06-11 10:28:27 -0300

    add rota de estatisticas dos filmes

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView
+from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView, MovieStatsView
 
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movie-create-list'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('movies/stats/', MovieStatsView.as_view(), name='movie-stats-view'),
 ]
